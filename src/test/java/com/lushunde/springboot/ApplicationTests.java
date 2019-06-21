c7530edcb10fa267ca5e99325c16c899dfcf5e0b
lushunde <lushunde321@163.com>
2019-06-22 00:58:00 +0800

    修改测试文件

--- src/test/java/com/lushunde/springboot/ApplicationTests.java
+++ src/test/java/com/lushunde/springboot/ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ComLushundeSpringbootApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
